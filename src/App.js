470e4ee58b18ff68b21822fd5de6cf07cb008f89
navinkbe7 <navin_ilango@trimble.com>
2019-10-15 13:20:16 -0500

    Working version of fireBase Hosting

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import logo from './logo.svg';
+import logo from './android-logo.png'; 
 import './App.css';
 
 function App() {
@@ -8,15 +8,15 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Welcome to Android
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://www.android.com/what-is-android/"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn Android
         </a>
       </header>
     </div>
